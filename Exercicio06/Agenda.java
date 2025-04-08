3c0ed2a4c07d1ad1b99455b4cecc631f9399a777
viniciusscss <viniciusdacostasilvasilva@gmail.com>
2025-04-08 20:07:11 -0300

    Adiciona exercício 06 resolvido e altera App.java

---
+++ Exercicio06/Agenda.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Agenda {
+    private ArrayList<Contato> contatos;
+
+    public Agenda() {
+        contatos = new ArrayList<>();
+    }
+
+    public void adicionarContato(Contato contato) {
+        contatos.add(contato);
+    }
+
+    public Contato buscarContatoPorNome(String nome) {
+        for (Contato contato : contatos) {
+            if (contato.getNome().equalsIgnoreCase(nome)) {
+                return contato;
+            }
+        }
+        return null;
+    }
+
+    public void exibirContatos() {
+        for (Contato contato : contatos) {
+            System.out.println(contato);
+        }
+    }
+}
